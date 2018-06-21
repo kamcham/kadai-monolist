9cb4cd28afb65d72bac7a0df6dfaebc3026ea89c
kanna.satake <blqblqblq666@gmail.com>
2018-06-21 04:24:37 +0000

    have

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
-                <h1>素敵なモノと出会う場所</h1>
+                <h1>しゅてきなモノと出会う場所</h1>
                 @if (!Auth::check())
                     <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
                 @endif
